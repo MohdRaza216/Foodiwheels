24fc3bfafc1f12dddcc00bdc3dc34bb2e2e7187a
MohdRaza216 <mohdrazamoghul2024@gmail.com>
2024-12-23 13:16:48 +0530

    Update templates for consistency

--- myweb/settings.py
+++ myweb/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from decouple import config
 import requests
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,11 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -139,4 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- website/urls.py
+++ website/urls.py
@@ -4,13 +4,13 @@ from website import views
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('contact', views.contact, name='contact'),
     path('about', views.about, name='about'),
     path('veg', views.veg, name='veg'),
     path('nonveg', views.nonveg, name='nonveg'),
     path('login', views.login, name='login'),
-
 
 ]
 
